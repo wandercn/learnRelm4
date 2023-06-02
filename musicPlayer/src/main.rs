9f2414be134ccb6baa366aef6a92af88251dc5c6
wander <wander@ffactory.org>
2023-06-02 17:58:29 +0800

    Update main.rs

--- musicPlayer/src/main.rs
+++ musicPlayer/src/main.rs
@@ -1,4 +1,4 @@
-use gtk::prelude::*;
+use gtk::{prelude::*, FileChooserDialog};
 use rand::prelude::*;
 use relm4::prelude::*;
 
@@ -9,6 +9,9 @@ enum Msg {
     Stop,
     Next,
     Prev,
+    ShowSearchBar,
+    CloseWindows,
+    // OpenFolder,
 }
 
 struct App {
@@ -16,6 +19,7 @@ struct App {
     volume: f64,
     play_mode: Modes,
     music_library: Option<Vec<Song>>,
+    search_mode_enable: bool,
 }
 
 enum Modes {
@@ -43,39 +47,99 @@ impl SimpleComponent for App {
     view! {
         #[root]
         gtk::Window {
-            gtk::Box{
-                set_orientation:gtk::Orientation::Vertical,
-                gtk::SearchEntry{
-                    set_placeholder_text:Some("搜索"),
-                        },
-                        gtk::Box{
-                            set_orientation:gtk::Orientation::Vertical,
+            set_width_request: 600,
+            set_height_request:400,
+             connect_destroy[sender]=> move |_| {sender.input(Msg::CloseWindows)},
+            gtk::Paned{
+                set_position:100,
+                set_size_request:(600,-1),
+                #[wrap(Some)]
+                set_start_child = &gtk::Frame{
 
+                set_size_request:(100,-1),
+                gtk::Box{
+                    set_margin_all:5,
+                    set_spacing:10,
+                    set_orientation:gtk::Orientation::Vertical,
 
-                            gtk::Button{
-                                set_height_request:48,
-                                set_width_request:48,
-                                set_icon_name:"media-optical-symbolic",
+                    gtk::Button{
+                        set_icon_name:"edit-find-symbolic",
+                        set_tooltip_markup:Some("搜索"),
+                        set_height_request:64,
+                        set_width_request:64,
 
+                        connect_clicked =>Msg::ShowSearchBar,
                             },
+                                set_orientation:gtk::Orientation::Vertical,
+
+
+                                gtk::Button{
+                                    set_height_request:64,
+                                    set_width_request:64,
+                                    set_icon_name:"media-optical-symbolic",
+                                    set_tooltip_markup:Some("音乐库"),
+                                    // connect_activate => println!("{}",css_name()),
+
+                                },
+                                gtk::Button{
+                                    set_height_request:64,
+                                    set_width_request:64,
+                                set_icon_name:"media-playlist-consecutive-symbolic",
+                                set_tooltip_markup:Some("播放列表"),
+                                }
+
+                },},
+                #[wrap(Some)]
+                set_end_child=&gtk::Frame{
+                    gtk::Box{
+                        set_orientation: gtk::Orientation::Vertical,
+                        gtk::Box{
+                            set_orientation: gtk::Orientation::Horizontal,
+                            // append = &filedialog,
+                            // set_height_request:20,
                             gtk::Button{
-                                set_height_request:48,
-                                set_width_request:48,
-                            set_icon_name:"media-playlist-consecutive-symbolic"
+                                set_icon_name:"folder-open-symbolic",
+                                // connect_clicked=>filedialog.clone()
                             }
                         },
+                    gtk::SearchBar{
+                        set_visible:true,
+                        #[watch]
+                        set_search_mode:model.search_mode_enable,
+                        // set_show_close_button:true,
+                    #[wrap(Some)]
+                        set_child = &gtk::SearchEntry{
+                            set_editable:true,
+                            set_enable_undo:true,
+                            set_placeholder_text: Some("搜索"),
+                            set_max_width_chars: 25,
+                       },
+                    },gtk::Label{
+                    set_label:"right",
+
+                },
             },
+                set_size_request:(200,-1),
+            },
+            },
+
 
         }
     }
 
-    fn update(&mut self, msg: Msg, _sender: ComponentSender<Self>) {
+    fn update(&mut self, msg: Msg, sender: ComponentSender<Self>) {
         match msg {
             Msg::Play => println!("Play"),
             Msg::Suspend => println!("Suspend"),
             Msg::Stop => println!("Stop"),
             Msg::Next => println!("Next"),
             Msg::Prev => println!("Prev"),
+            Msg::ShowSearchBar => {
+                println!("{}", self.search_mode_enable);
+                self.search_mode_enable = !self.search_mode_enable;
+            }
+            Msg::CloseWindows => relm4::main_application().quit(),
+            // Msg::OpenFolder => ,
         }
     }
 
@@ -89,7 +153,17 @@ impl SimpleComponent for App {
             volume: 0.4,
             play_mode: Modes::Order,
             music_library: None,
+            search_mode_enable: false,
         };
+        let openFile = FileChooserDialog::builder();
+
+        let filedialog = openFile
+            .select_multiple(true)
+            .title(glib::gstr!("Open file"))
+            .visible(true)
+            .action(gtk::FileChooserAction::Open)
+            .build();
+
         let widgets = view_output!();
 
         ComponentParts { model, widgets }
@@ -98,5 +172,6 @@ impl SimpleComponent for App {
 
 fn main() {
     let app = RelmApp::new("player.ffactory.org");
+
     app.run::<App>(());
 }
